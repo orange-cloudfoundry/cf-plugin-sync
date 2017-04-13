f707bd3e9042ef3ea330eab118e78cf41f9924f0
ArthurHlt <arthur.halet@orange.com>
2017-04-14 01:49:53 +0200

    add color for windows

--- logger_windows.go
+++ logger_windows.go
@@ -4,8 +4,8 @@ package main
 
 import (
 	"github.com/ArthurHlt/gominlog"
-	"os"
 	"log"
+	"github.com/fatih/color"
 )
 
-var logger *gominlog.MinLog = gominlog.NewMinLogWithWriter("cf-sync", gominlog.Linfo, false, log.Ldate | log.Ltime, os.Stdout)
+var logger *gominlog.MinLog = gominlog.NewMinLogWithWriter("cf-sync", gominlog.Linfo, true, log.Ldate | log.Ltime, color.Output)
